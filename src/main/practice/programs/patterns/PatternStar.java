4c0d4eadf7ac3dfccc66d52bfe9691ff1832d9a8
SSdCode <dubeysamir514@gmail.com>
2023-12-25 11:51:16 +0530

    add more pattern programs.

--- src/main/practice/programs/patterns/PatternStar.java
+++ src/main/practice/programs/patterns/PatternStar.java
@@ -117,7 +117,7 @@ public class PatternStar {
          *       *
          * * * * *
          */
-        int noRows = 4, noCols=5;
+        int noRows = 4, noCols = 5;
         for (int i = 1; i <= noRows; i++) {
             for (int j = 1; j <= noCols; j++) {
                 if (i == 1 || i == noRows || j == 1 || j == noCols)
@@ -127,5 +127,183 @@ public class PatternStar {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+
+        // Inverted & rotated half pyramid
+        /*
+
+            *
+           **
+          ***
+         ****
+
+         * */
+        int no = 4;
+        for (int i=1;i<=no;i++){
+            for (int j=1;j<=no;j++){
+                if (j>no-i)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        // Inverted - half pyramid
+        /*
+
+         12345
+         1234
+         123
+         12
+         1
+
+         * */
+        no=5;
+        for (int i = no; i >= 1; i--) {
+            for (int j = 1; j <= no; j++) {
+                if (j <= i)
+                    System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+
+        // FLOYD's Triangle pattern
+        /*
+
+         1
+         2 3
+         4 5 6
+         7 8 9 10
+         11 12 13 14 15
+
+         * */
+
+        noRows = 5; no = 1;
+        for (int i = 1; i <= noRows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(no+" ");
+                no++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+// 0-1 triangle pattern
+        /*
+
+         1
+         01
+         101
+         0101
+         10101
+
+         * */
+        int mNo=1;
+        for (int i = 1; i <= noRows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 && i % 2 == 0) {
+                    mNo = 0;
+                } else if (j == 1) {
+                    mNo = 1;
+                }
+                System.out.print(mNo);
+                if (mNo == 0)
+                    mNo = 1;
+                else
+                    mNo = 0;
+            }
+
+            System.out.println();
+        }
+
+        // Butterfly pattern
+        /*
+
+         *      *
+         **    **
+         ***  ***
+         ********
+         ********
+         ***  ***
+         **    **
+         *      *
+
+         */
+
+
+        // Solid rhombus pattern
+        /*
+             *****
+            *****
+           *****
+          *****
+         *****
+
+         */
+
+
+        // Hollow rhombus pattern
+        /*
+
+             *****
+            *   *
+           *   *
+          *   *
+         *****
+
+         */
+
+        // Diamond pattern
+
+        /*
+
+           *
+          ***
+         *****
+        *******
+         *****
+          ***
+           *
+
+         * */
+
+
+        //Number pyramid pattern
+
+        /*
+            1
+           2 2
+          3 3 3
+         4 4 4 4
+        5 5 5 5 5
+
+        * */
+
+
+
+        // Palindromic pattern
+
+        /*
+
+            1
+           212
+          32123
+         4321234
+        543212345
+
+        */
+
+
     }
 }
