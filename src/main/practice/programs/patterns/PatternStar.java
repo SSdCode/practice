c4aa87649c719cf8e605ad91ece241d02ffd323a
SSdCode <dubeysamir514@gmail.com>
2023-12-25 10:46:19 +0530

    pattern add.

---
+++ src/main/practice/programs/patterns/PatternStar.java
@@ -0,0 +1,132 @@
+package programs.patterns;
+
+public class PatternStar {
+    public static void main(String[] args) {
+        /*
+         *****
+         *****
+         *****
+         *****
+         *****
+         * */
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        /*
+         *
+         **
+         ***
+         ****
+         *****
+         */
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        /*
+         *****
+         ****
+         ***
+         **
+         *
+         * */
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        //Half pyramid pattern
+
+        /*
+        1
+        12
+        123
+        1234
+        * */
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        //Character pattern
+
+        /*
+        A
+        BC
+        DEF
+        GHIJ - 65 - 74
+        * */
+        char ch = 'A';
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k <= j; k++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        /*
+        4444444
+        4333334
+        4322234
+        4321234
+        */
+
+        for (int i = 4; i > 0; i--) {
+            for (int j = 4; j > 0; j--) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+
+
+        // Hollow Rectangle pattern
+        /*
+         * * * * *
+         *       *
+         *       *
+         * * * * *
+         */
+        int no = 5;
+        for (int i = 1; i < no; i++) {
+            for (int j = 1; j <= no; j++) {
+                if (i == 1 || i == no - 1 || j == 5 || j == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
